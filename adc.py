05fd718c692727e74f8c7acf57ab4d7fbfad49f8
Anson Lau <laux8730@mylaurier.ca>
2022-11-10 23:52:11 +0000

    first commit yay!

---
+++ adc.py
@@ -0,0 +1,16 @@
+import smbus
+import time
+address = 0x48
+A0 = 0x40 #photoresistor
+A1 = 0x41 #nothing
+A2 = 0x42 #thermistor
+A3 = 0x43 #ptentiometer
+bus = smbus.SMBus(1)
+
+while True:
+    bus.write_byte(address,A3)
+    value = bus.read_byte(address)
+    print(value)
+    time.sleep(0.1)
+
+
---
+++ servo.py
@@ -0,0 +1,27 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+
+THEORETICAL_LEFT = 1.0/20.0 * 100 # by calculation (1ms / 20ms * 100)
+THEORETICAL_RIGHT = 2.0/20.0 * 100 # by calculation (2ms / 20ms * 100)
+LEFT = 0.5/20.0 * 100 # by testing (0.5ms)
+RIGHT = 2.5/20.0 * 100 # by testing (2.5ms)
+MIDDLE = 1.5/20.0 * 100
+p = GPIO.PWM(17, 50)  # channel=18 frequency=50Hz
+p.start(MIDDLE)
+time.sleep(1)
+try:
+    while True:
+            p.ChangeDutyCycle(LEFT)
+            time.sleep(1)
+            p.ChangeDutyCycle(RIGHT)
+            time.sleep(1)
+except KeyboardInterrupt:
+    pass
+
+time.sleep(1)
+p.ChangeDutyCycle(MIDDLE)
+time.sleep(1)
+p.stop()
+GPIO.cleanup()
