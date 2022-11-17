15f772b0dfb3e8dfea9ce9da53c5c2b94d0ee76b
Anson Lau <laux8730@mylaurier.ca>
2022-11-17 20:57:59 +0000

    adc.py remade

--- adc.py
+++ adc.py
@@ -6,11 +6,8 @@ A1 = 0x41 #nothing
 A2 = 0x42 #thermistor
 A3 = 0x43 #ptentiometer
 bus = smbus.SMBus(1)
-
 while True:
     bus.write_byte(address,A3)
     value = bus.read_byte(address)
     print(value)
     time.sleep(0.1)
-
-
