fd477d436305f3278312e1e6f8ad1ea7f3f72e07
Anson Lau <laux8730@mylaurier.ca>
2022-12-03 22:39:28 +0000

    edited function names

--- integration.py
+++ integration.py
@@ -24,20 +24,20 @@ ADDRESS = 0X48
 pot = 0x43
 
 #Global variables
-FULL_ROTATION = 360
-STEP_ANGLE = 5.625
+
 LEFT = 0.5/20.0 * 100 
 RIGHT = 2.5/20.0 * 100 
 MIDDLE = 1.5/20.0 * 100  # by calculation (1.5ms / 20ms * 100)
 DEG = (RIGHT - LEFT) / 190.0 # assume range of server is ~190deg
 SONIC_SPEED = 34300
+MIN_ULTRASONIC_DIST = 20
+MIN_POT_VAL = 0
+MIDDLE_POT_VAL = 125
 
 #GPIO setup
-
 GPIO.setup(gpioServo, GPIO.OUT)
 GPIO.setup(gpioTrig, GPIO.OUT)
 GPIO.setup(gpioEcho, GPIO.IN)
-
 GPIO.setup(stepper_pins, GPIO.OUT)
 
 
@@ -46,8 +46,8 @@ p = GPIO.PWM(gpioServo, freq)
 
 
 
-
-def stepperMotor():
+#definitions
+def stepperMotorInit():
     step_sequence = []
     step_sequence.append([1, 0, 0, 0])
     step_sequence.append([0, 1, 1, 0])
@@ -56,15 +56,14 @@ def stepperMotor():
 
     return step_sequence
 
-def servoMotor(direction, direction2):
+def servoMotorDefault(direction, direction2):  
     p.start(MIDDLE)
     p.ChangeDutyCycle(direction)
     time.sleep(1)
     p.ChangeDutyCycle(direction2)
     time.sleep(1)
 
-def ultrasonic():
-
+def ultrasonicDist():
   GPIO.output(gpioTrig, True)
 
 
@@ -90,7 +89,7 @@ def ultrasonic():
 
   return distance
 
-def potentiometer():
+def potentiometerControl():
     bus = smbus.SMBus(1)
     bus.write_byte(ADDRESS,pot)
     value = bus.read_byte(ADDRESS)
@@ -102,54 +101,55 @@ def servoAngle(value, minD, maxD, minA, maxA):
   output = minA + (float(value - minD) / float(maxD - minD) * (maxA - minA))
   return max(min(output, maxA), minA)
 
+try:
+    response = " "
+    while (response != "servo" and response != "stepper"):
+        response = input("Please enter servo or stepper: ")
 
-response = " "
-while (response != "servo" and response != "stepper"):
-    response = input("Please enter servo or stepper: ")
+    if( response == 'servo'):
 
-if( response == 'servo'):
-
-    while True:
-        
-        value = potentiometer()
-        print("current potentiometer value: ", value)
-        if(value >= 0 and value < 101):                  #automatically rotates if pot value is between 0 and 100
-            servoMotor(RIGHT,LEFT)
-        else:                                            #uses ultrasonic to detect a value to generate a certain angle for the servo to move; basically moves servo based on ultrasonic detection
-            distance = ultrasonic()
-            degrees = servoAngle(distance, 0, 30, 0, 190)
-            angle = LEFT + DEG*degrees
-            print("distance: ", distance)
-            print("angle: ",angle)
-            p.ChangeDutyCycle(angle)
-            time.sleep(1)
-
-elif(response == 'stepper'):
-    
-    while True:
-        dist = ultrasonic()
-        print("ultrasonic dist: ", dist)
-        steps = stepperMotor()
-        if(dist < 20):
+        while True:
             
-                
-            for row in (steps): 
-                GPIO.output(stepper_pins,row)
-                time.sleep(0.01)
-        else:
+            value = potentiometerControl()
+            print("current potentiometer value: ", value)
+
+            if(value >= MIN_POT_VAL and value < MIDDLE_POT_VAL):      #automatically rotates if pot value is between MIN_POT_VAL = 0 and MIDDLE_POT_VAL = 101
+                servoMotorDefault(RIGHT,LEFT)
+            else:                        # if potentiometer value is > 101; uses ultrasonic to detect a value to generate a certain angle for the servo to move; basically moves servo based on ultrasonic detection
+                distance = ultrasonicDist()
+                degrees = servoAngle(distance, 0, 30, 0, 190)
+                angle = LEFT + DEG*degrees
+                print("distance: ", distance)
+                print("angle: ",angle)
+                print(" ")
+                p.ChangeDutyCycle(angle)
+                time.sleep(1)
+
+    elif(response == 'stepper'): #user selects to use stepper motor
         
-            for row in reversed (steps):
-                    
-                GPIO.output(stepper_pins,row)
-                time.sleep(0.01)
-        time.sleep(0.01)
+        while True: #will rotate in a direction based on distance detected by ultrasonic
+            dist = ultrasonicDist()
+            print("ultrasonic dist: ", dist)
+            steps = stepperMotorInit()
 
+            if(dist < MIN_ULTRASONIC_DIST): #MIN_ULTRASONIC_DIST = 20; 
+                
+                for row in (steps): 
+                    GPIO.output(stepper_pins,row)
+                    time.sleep(0.01)
+            else:
+            
+                for row in reversed (steps):
+                        
+                    GPIO.output(stepper_pins,row)
+                    time.sleep(0.01)
+            time.sleep(0.01)
 
-    
-
-    
-
-GPIO.cleanup()
+except KeyboardInterrupt:
+    print("\nprogram stopped by user")
+    p.ChangeDutyCycle(MIDDLE)
+    p.stop
+    GPIO.cleanup()
 
 
 
