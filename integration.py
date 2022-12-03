39cd6f0a915154cd2390b4f461b86084f187c241
Anson Lau <laux8730@mylaurier.ca>
2022-12-03 20:56:58 +0000

    finished test stepper feature

--- integration.py
+++ integration.py
@@ -26,8 +26,8 @@ pot = 0x43
 #Global variables
 FULL_ROTATION = 360
 STEP_ANGLE = 5.625
-LEFT = 0.5/20.0 * 100 # by testing (0.5ms)
-RIGHT = 2.5/20.0 * 100 # by testing (2.5ms)
+LEFT = 0.5/20.0 * 100 
+RIGHT = 2.5/20.0 * 100 
 MIDDLE = 1.5/20.0 * 100  # by calculation (1.5ms / 20ms * 100)
 DEG = (RIGHT - LEFT) / 190.0 # assume range of server is ~190deg
 SONIC_SPEED = 34300
@@ -37,12 +37,12 @@ GPIO.setup(gpioServo, GPIO.OUT)
 GPIO.setup(gpioTrig, GPIO.OUT)
 GPIO.setup(gpioEcho, GPIO.IN)
 GPIO.setup(stepper_pins, GPIO.OUT)
-GPIO.setwarnings(False)
+
 
 #set value p as pulse width modulation
 p = GPIO.PWM(gpioServo, freq)
 
-#bus = bus.SMBus(1)
+
 
 
 def stepperMotor():
@@ -52,9 +52,7 @@ def stepperMotor():
     step_sequence.append([0, 0, 1, 1])
     step_sequence.append([1, 0, 0, 1])
 
-    for row in step_sequence:
-        GPIO.output(stepper_pins, row)
-        time.sleep(1)
+    return step_sequence
 
 def servoMotor(direction, direction2):
     p.start(MIDDLE)
@@ -64,10 +62,10 @@ def servoMotor(direction, direction2):
     time.sleep(1)
 
 def ultrasonic():
-    # set Trigger to HIGH
+
   GPIO.output(gpioTrig, True)
 
-  # set Trigger after 0.01ms to LOW
+
   time.sleep(0.00001)
   GPIO.output(gpioTrig, False)
 
@@ -82,7 +80,7 @@ def ultrasonic():
   while GPIO.input(gpioEcho) == 1:
       StopTime = time.time()
 
-  # time difference between start and arrival
+
   TimeElapsed = StopTime - StartTime
   # multiply with the sonic speed (34300 cm/s)
   # and divide by 2, because there and back
@@ -98,7 +96,7 @@ def potentiometer():
     return value
 
 
-def mapToAngle(value, minD, maxD, minA, maxA):
+def servoAngle(value, minD, maxD, minA, maxA):
   output = minA + (float(value - minD) / float(maxD - minD) * (maxA - minA))
   return max(min(output, maxA), minA)
 
@@ -115,13 +113,25 @@ if( response == 'servo'):
         if(value >= 0 and value < 101):
             servoMotor(RIGHT,LEFT)
         else:
-            dist = ultrasonic()
-            degrees = mapToAngle(dist, 0, 30, 0, 190)
+            distance = ultrasonic()
+            degrees = servoAngle(distance, 0, 30, 0, 190)
             angle = LEFT + DEG*degrees
             p.ChangeDutyCycle(angle)
-        
-        
 
+elif(response == 'stepper'):
+    response2 = ' '
+    while(response2 != "cw" and response2 != "ccw"):
+        response2 = input("please enter cw or ccw (clockwise or counter-clockwise): ")
+        steps = stepperMotor()
+
+        if(response2 == "cw"):
+            for row in steps:
+                GPIO.output(step_pins,row)
+                time.sleep(0.001)
+        elif(response2 == "ccw"):
+            for row in reversed steps:
+                GPIO.output(step_pins,row)
+                time.sleep(0.001)
 
 '''
 while True:
